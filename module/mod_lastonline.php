a0a69b728312b75ec61ef29f7481d725a9c24e89
Anton <anton@nazvezde.ru>
2016-09-02 13:12:41 +0300

    Tooltips_temp

--- module/mod_lastonline.php
+++ module/mod_lastonline.php
@@ -6,8 +6,8 @@ function lastOnline() {
     for ($i = 1; $i < 13; $i++) {
         if ($i != 5 && $i != 11) {
             $query .= "
-            SELECT `temp`.`did`, `temp`.`name`, CEIL(AVG(`temp`.`status`)) as `status`, MAX(`temp`.`status`) as `max` FROM (
-            (SELECT `d`.`id` as `did`, `d`.`name` as `name`, `r`.`status` as `status`
+            SELECT `temp`.`did`, `temp`.`name`, CEIL(AVG(`temp`.`status`)) as `status`, MAX(`temp`.`status`) as `max`, `number`, `route` FROM (
+            (SELECT `d`.`id` as `did`, `d`.`name` as `name`, `r`.`status` as `status`, `r`.`num` as `number`, `r`.`route` as `route`
             FROM `#__rasp_online` as `r`
             LEFT JOIN `#__directions` as `d` ON (`r`.`dir`=`d`.`id`)
             WHERE `dir` = $i AND `dat`=CURRENT_DATE() AND `r`.`arr`=(SELECT MAX(`arr`) FROM `#__rasp_online` WHERE `dir` = $i AND `dat`=CURRENT_DATE() AND `num`=`r`.`num`)
@@ -36,16 +36,20 @@ $opozdaniya = lastOnline();
         <tbody>
 <?php
 foreach ($opozdaniya as $dir) {
-    $status = $dir->status == '0' ? JText::_('MOD_LASTONLINE_STATUS_OK') : $dir->status.' '.JText::_('MOD_LASTONLINE_ARR_MINUTE');
-    $status_max = $dir->max == '0' ? JText::_('MOD_LASTONLINE_STATUS_OK') : $dir->max.' '.JText::_('MOD_LASTONLINE_ARR_MINUTE');
-    $url_dir = '/rasp/online?dir='.$dir->did.'&date='.date("Y-m-d");
-    ?>
-    <tr>
-        <td><a href="<?=$url_dir?>"><?=$dir->name?></a></td>
-        <td><?=$status?></td>
-        <td><?=$status_max?></td>
-    </tr>
-    <?php
+    if ($dir) {
+        $status = $dir->status == '0' ? JText::_('MOD_LASTONLINE_STATUS_OK') : $dir->status.' '.JText::_('MOD_LASTONLINE_ARR_MINUTE');
+        $status_max = $dir->max == '0' ? JText::_('MOD_LASTONLINE_STATUS_OK') : $dir->max.' '.JText::_('MOD_LASTONLINE_ARR_MINUTE');
+        $url_dir = '/rasp/online?dir='.$dir->did.'&date='.date("Y-m-d");
+        ?>
+        <tr>
+            <td><a href="<?=$url_dir?>"><?=$dir->name?></a></td>
+            <td><?=$status?></td>
+            <td>
+                <?=$status_max?>
+            </td>
+        </tr>
+        <?php
+    }
 }
 ?>
         </tbody>
